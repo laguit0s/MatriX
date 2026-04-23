6de6a3d5375f4e69d1564ee6ae9b801314616985
Guilherme <guilhermelacerda146@gmail.com>
2026-04-23 00:20:21 -0300

    ajusta tabela de dados

--- frontend/src/components/DataTable.jsx
+++ frontend/src/components/DataTable.jsx
@@ -1,39 +1,40 @@
-function DataTable({ qtdHead, contentHead, qtdBody, contentBody }) {
-    let qtdRows = qtdBody / qtdHead;
+function DataTable({ headerContent = [], bodyContent = [] }) {
+    const qtdHeader = headerContent.length;
+    const qtdRows = qtdHeader > 0 ? Math.ceil(bodyContent.length / qtdHeader) : 0;
     
     return (
         <div className="table-responsive flex-grow-1 w-100" style={{ minWidth: 0 }}>
             <table className="table table-striped text-nowrap">
                 <thead>
                     <tr>
-                        {
-                            Array.from({length: qtdHead}).map((e, i) => {
-                                e = contentHead[i];
-                                return <th key={i} className="sticky-top">{e}</th>
-                            })
-                        }
+                        {headerContent.map((texto, i) => (
+                            <th key={i} className="sticky-top">{texto}</th>
+                        ))}
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        Array.from({length: qtdRows}).map((_, i) => {
-                            return (
-                                <tr key={i}>
-                                    {
-                                        Array.from({length: qtdHead}).map((e, j) => {
-                                            let index = qtdHead * i + j;
-                                            e = contentBody[index];
-                                            return <td key={index}>{e}</td>
-                                        })
-                                    }
-                                </tr>
-                            )
-                        })
+                        Array.from({ length: qtdRows }).map((_, i) => (
+                            <tr key={i}>
+                                {
+                                    Array.from({ length: qtdHeader }).map((_, j) => {
+                                        const index = qtdHeader * i + j;
+                                        const cellContent = bodyContent[index];
+                                        
+                                        return (
+                                            <td key={index}>
+                                                {cellContent !== undefined ? cellContent : ""}
+                                            </td>
+                                        );
+                                    })
+                                }
+                            </tr>
+                        ))
                     }
                 </tbody>
             </table>
         </div>
-    )
+    );
 }
 
-export default DataTable
+export default DataTable;
